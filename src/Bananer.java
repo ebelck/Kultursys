36127212dfea451457abc189cd999b28d17b0717
halvorR <halvorr91@gmail.com>
2015-04-22 14:21:34 +0200

    update bannan

--- src/Bananer.java
+++ src/Bananer.java
@@ -3,5 +3,7 @@ public class Bananer {
 public static void main(String[] args){
 	String bannan = "Bannan";
 	System.out.println(bannan);
+	int antallbannan = 1;
+	System.out.println(antallbannan);
 }
 }
