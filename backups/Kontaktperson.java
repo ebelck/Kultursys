b22e838b06aa29cddf35e4a1400b3174173e1f55
Roger Bløtekjær Johannessen <blawtekjar@gmail.com>
2015-04-25 10:42:16 +0200

    Klasser i hierarkiet fra Arrangement og ned

---
+++ backups/Kontaktperson.java
@@ -0,0 +1,42 @@
+
+public class Kontaktperson {
+	private String navn;
+	private String epost;
+	private String tlf;
+
+	public Kontaktperson (String n, String e, String t) {
+		navn = n;
+		epost = e;
+		tlf = t;
+	}
+	
+	
+	 /*//////////////////////
+	 Get og Set metoder start
+	 *//////////////////////
+	public void set_Navn(String n) {
+		navn = n;
+	}
+	public void set_Epost(String e) {
+		epost = e;
+	}
+	public void set_Tlf(String t) {
+		tlf = t;
+	}
+	public String get_Navn() {
+		return navn;
+	}
+	public String get_Epost() {
+		return epost;
+	}
+	public String get_Tlf() {
+		return tlf;
+	}
+	 /*//////////////////////
+	 Get og Set metoder finish
+	 *//////////////////////
+	
+	public String toString() {
+		return navn + "- " + epost + "- " + tlf;
+	}
+}
