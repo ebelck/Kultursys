3fc8a119b59489c4ae93ab467f88e72501458347
halvorR <halvorr91@gmail.com>
2015-05-07 21:46:47 +0200

    Lagt til Arraylist i Billettregister
    For lettere å manipulere lister, samt optimalisering av registeret

--- src/Billettregister.java
+++ src/Billettregister.java
@@ -1,102 +1,60 @@
 import java.io.Serializable;
+import java.util.*;
+
 
 public class Billettregister implements Serializable{
 	
-	private Billett første;
-	private Kino k;
-	private int nummer;
+	private List<Billett> reg = new ArrayList<Billett>();
 	
-	public Billettregister() {
-		første = null;
+	private int nesteNr;
+	private int antSolgt;
+	
+	/////////////////////////////////
+	////// Billettmanipulering //////
+	
+	public void settInnBillett( Billett b) {
+		reg.add(b);
 	}
 	
-	public boolean leggTilBillett(Billett b) {
-		if(b == null){
-			return false;
-		}
-		
-		if(første == null){
-			første = b;
-		} else {
-			Billett peker = første;
-			while(peker.neste != null)
-				peker = peker.neste;
-		
-			peker.neste = b;
-		}
-		return true;
+	public int antallSolgteBilletter() {
+		antSolgt = reg.size();
+		return antSolgt;
 	}
 	
-	public int nesteLedigeSete(){
-		if(første == null)
-			return nummer;
-		
-		Billett peker = første;
-		while(peker.neste != null) {
-			peker = peker.neste;
-		}
-		peker.get_Setenummer();
-		nummer = peker.get_Setenummer() + 1;
-		return nummer;
+	public int nesteLedigeBillett() {
+		nesteNr = reg.size() + 1;
+		return nesteNr;
 	}
 	
-	public int antallSolgteBilletter(){
-		if(første == null)
-			return nummer;
-		
-		Billett peker = første;
-		while(peker.neste != null) {
-			peker = peker.neste;
+	
+	public Billett finnBillett(int n) {
+		n = n - 1;
+		try {
+			Billett funnet;
+			funnet = reg.get(n);
+			return funnet;
+		} catch(IndexOutOfBoundsException IOOBE){
+			return null;
 		}
-		peker.get_Setenummer();
-		nummer = peker.get_Nummer();
-		return nummer;
 	}
 	
 	public boolean slettBillett(int n){
-		if(første == null)
-			return false;
-		
-		if(første.get_Nummer() == n){
-			første = første.neste;
+		n = n - 1;
+		try {
+			reg.remove(n);
 			return true;
+		} catch (IndexOutOfBoundsException IOOBE) {
+			return false;
 		}
-		
-		Billett peker = første;
-		while(peker.neste != null){
-			if(peker.neste.get_Nummer() == n){
-				peker.neste = peker.neste.neste;
-				return true;
-			}
-			peker = peker.neste;
-		}
-		return false;	
 	}
 	
-	public Billett finnBillett(int n){
-		if(første == null)
-			return null;
-		
-		Billett peker = første;
-		while (peker != null){
-			if(peker.get_Nummer() == n)
-				return peker;
-			peker = peker.neste;
+	// Billettmanipulering slutt //
+	///////////////////////////////
+	public String toString() {
+		String melding = "";
+		for (Billett s : reg) {
+			melding += s.toString();
 		}
-		return null;
-	}	
-	
-	public String toString(){
-		String svar = "Billetter:\r\n";
-		if(første == null)
-			return svar += "* Ingen billetter kjøpt"; 
-		
-		Billett peker = første;
-		while(peker != null){
-			svar += "* " + peker.toString() + "\r\n";
-			peker = peker.neste;
-		}
-		return svar;
+		return melding;
 	}
-	
-}
+} // Billettregister-klasse slutt
