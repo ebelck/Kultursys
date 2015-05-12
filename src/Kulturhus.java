82ebaa8a3298024c5bf50bd095526ec71b315de4
halvorR <halvorr91@gmail.com>
2015-05-12 15:13:01 +0200

    Gjort endringene i TODO, samt legt til committen jeg reverta

--- src/Kulturhus.java
+++ src/Kulturhus.java
@@ -65,7 +65,11 @@ public class Kulturhus {
 	public String listArrangementerILokaler() {
 		String melding = "";
 		for (Lokale s : LokalerInhouse) {
-			melding += s.listArrangmenter();
+			melding += "LOKALE:\t" + s.get_Navn() + "\r\n";
+			if(!s.tomtRegister())
+				melding += s.listArrangementer();
+			else
+				melding += "Ingen arrangementer\r\n";
 		}
 		return melding;
 	}
